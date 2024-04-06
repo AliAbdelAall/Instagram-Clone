3884360e804432d3d405a87b8df07c0c8213c671
AliAbdelAall <3laych@gmail.com>
2024-04-06 22:19:06 +0300

    added migration table follows

---
+++ backend/database/migrations/2024_04_06_190808_create-follows-table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('follows', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger("followed_id");
+            $table->foreign('follower_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->unsignedBigInteger('followed_id');
+            $table->foreign('followed_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
