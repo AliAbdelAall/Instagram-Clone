c675e7b0255a8e9a1a6c899bfc09e6a9e5eb8a40
AliAbdelAall <3laych@gmail.com>
2024-04-06 23:52:20 +0300

    added UserSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,37 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'username' => "Ali",
+            'email' => "ali@gmail.com",
+            'password' => Hash::make("ali12345"),
+            'full_name' => "Ali Abdelaal",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+        ]);
+        User::create([
+            'username' => "Ahmad",
+            'email' => "ahmad@gmail.com",
+            'password' => Hash::make("ahmad12345"),
+            'full_name' => "Ahmad Farou",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+        ]);
+        User::create([
+            'username' => "Mhamad",
+            'email' => "mhamad@gmail.com",
+            'password' => Hash::make("mhamad12345"),
+            'full_name' => "Mhamad Ballan",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+        ]);
+        User::create([
+            'username' => "Hsein",
+            'email' => "hsein@gmail.com",
+            'password' => Hash::make("hsein12345"),
+            'full_name' => "Hsein Taktak",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+        ]);
     }
 }
