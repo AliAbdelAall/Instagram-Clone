4ce801794f595a3dcbe023caf4a3f6fa7f7b53eb
AliAbdelAall <3laych@gmail.com>
2024-04-07 19:57:28 +0300

    added Input Component

--- frontend/src/pages/Authentication/components/Login/index.jsx
+++ frontend/src/pages/Authentication/components/Login/index.jsx
@@ -4,14 +4,25 @@ import {useLocation, Link} from "react-router-dom"
 // styles
 import "./style.css"
 
+// components
+import Input from '../../../../components/Input'
+
 
 const Login = () => {
   return (
     <div className='flex column center login-wrapper'>
       <div className='flex column center input-wrapper'>
         <h1>Instagram</h1>
-        <input type="text" placeholder='Username, or email'/>
-        <input type="password" placeholder='Password'/>
+        <Input
+        type={"text"}
+        placeholder={'Username, or email'}
+        // handleChange={}
+        />
+        <Input
+        type={"password"}
+        placeholder={'Password'}
+        // handleChange={}
+        />
         <button className='login-btn white bold text-sm bg-primary'>Log in</button>
       </div>
       <div className='flex center  login-switch'>
