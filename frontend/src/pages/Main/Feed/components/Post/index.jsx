f921c19835aa0139573e02efa2e2448326278d72
AliAbdelAall <3laych@gmail.com>
2024-04-09 12:02:20 +0300

    added Post component

---
+++ frontend/src/pages/Main/Feed/components/Post/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import heart from "../../../../../assets/icons/heart.svg"
+import heartRed from "../../../../../assets/icons/heart-red.svg"
+import commentIcon from "../../../../../assets/icons/comment.svg"
+
+const Post = ({profileImage, postImage, liked, likes, caption, myComment,setComment, setLiked}) => {
+  return (
+    <div className='flex column post-wrapper'>
+
+          <div className='flex align-center user_info-post'>
+            <img src={profileImage} width={32} hight={32} alt="profile" />
+            <p className='username-post bold text-sm'>username</p>
+          </div>
+
+          <img src={postImage} alt="Post" />
+
+          <div className='flex like-comment'>
+            {liked ? 
+            (<img src={heartRed} alt="heart-red" onClick={() => setLiked(false)}/>) :
+            (<img src={heart} alt="heart" onClick={() => setLiked(true)}/>)
+            }
+            <img src={commentIcon} alt="comment" />
+          </div>
+
+          <p className='post_likes text-sm'>{likes} likes</p>
+          <p className='post-caption text-sm'>{caption}</p>
+
+          <div className='flex space-between comment-post'> 
+            <textarea
+            className='text-sm'
+            type={"text"}
+            placeholder={"Add a comment"}
+            onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+
+            {myComment && <p className='text-sm text-primary'>Post</p>}
+          </div> 
+          <div className='sparator bg-gray-col'></div>
+        </div>
+  )
+}
+
+export default Post
--- frontend/src/pages/Main/Feed/index.jsx
+++ frontend/src/pages/Main/Feed/index.jsx
@@ -6,9 +6,9 @@ import "./style.css"
 // Images
 import defaultProfile from "../../../assets/profile/Default_pfp.jpg"
 import postImage from "../../../assets/other/background3.jpg"
-import heart from "../../../assets/icons/heart.svg"
-import heartRed from "../../../assets/icons/heart-red.svg"
-import commentIcon from "../../../assets/icons/comment.svg"
+
+// components
+import Post from './components/Post'
 
 const Feed = () => {
   const [liked, setLiked] = useState(false)
@@ -17,38 +17,16 @@ const Feed = () => {
   return (
     <div className='flex center feed-continer'>
       <div className='flex column posts-container'>
-        <div className='flex column post-wrapper'>
-
-          <div className='flex align-center user_info-post'>
-            <img src={defaultProfile} width={32} hight={32} alt="profile" />
-            <p className='username-post bold text-sm'>username</p>
-          </div>
-
-          <img src={postImage} alt="Post" />
-
-          <div className='flex like-comment'>
-            {liked ? 
-            (<img src={heartRed} alt="heart-red" onClick={() => setLiked(false)}/>) :
-            (<img src={heart} alt="heart" onClick={() => setLiked(true)}/>)
-            }
-            <img src={commentIcon} alt="comment" />
-          </div>
-
-          <p className='post_likes text-sm'>12312 likes</p>
-          <p className='post-caption text-sm'>this is my caption it should be a little long so we can test the word break inside the post</p>
-
-          <div className='flex space-between comment-post'> 
-            <textarea
-            className='text-sm'
-            type={"text"}
-            placeholder={"Add a comment"}
-            onChange={(e) => setComment(e.target.value)}
-            ></textarea>
-
-            {comment && <p className='text-sm text-primary'>Post</p>}
-          </div> 
-          <div className='sparator bg-gray-col'></div>
-        </div>
+        <Post
+        profileImage={defaultProfile} 
+        postImage={postImage}
+        setComment={setComment}
+        setLiked={setLiked} 
+        liked={liked} 
+        likes={243} 
+        caption={"this is my caption it should be a little long so we can test the word break inside the post"} 
+        myComment={comment}
+        />
        
       </div>
       <div className='flex column align-self-start profile-sugestions'>
