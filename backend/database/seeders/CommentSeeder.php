e549279d85888fa62e9fae421ad57d6e14c42e4f
AliAbdelAall <3laych@gmail.com>
2024-04-09 03:47:36 +0300

    added CommentSeeder

--- backend/database/seeders/CommentSeeder.php
+++ backend/database/seeders/CommentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,52 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Comment::create([
+            'user_id' => 1, 
+            'post_id' => 2, 
+            'comment' => 'Great post!'
+        ]);
+
+        Comment::create([
+            'user_id' => 2, 
+            'post_id' => 1, 
+            'comment' => 'Nice!'
+        ]);
+
+        Comment::create([
+            'user_id' => 3, 
+            'post_id' => 2, 
+            'comment' => 'Awesome!'
+        ]);
+
+        Comment::create([
+            'user_id' => 4, 
+            'post_id' => 3, 
+            'comment' => 'Amazing!'
+        ]);
+
+        Comment::create([
+            'user_id' => 5, 
+            'post_id' => 1, 
+            'comment' => 'Fantastic!'
+        ]);
+
+        Comment::create([
+            'user_id' => 6, 
+            'post_id' => 4, 
+            'comment' => 'Incredible!'
+        ]);
+
+        Comment::create([
+            'user_id' => 1, 
+            'post_id' => 5, 
+            'comment' => 'Impressive!'
+        ]);
+
+        Comment::create([
+            'user_id' => 2, 
+            'post_id' => 6, 
+            'comment' => 'Brilliant!'
+        ]);
     }
 }
