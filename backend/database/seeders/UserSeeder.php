7a5b25f9f87db02a9bca0b3b5e302afdb03ba8a6
AliAbdelAall <3laych@gmail.com>
2024-04-09 03:51:54 +0300

    added data to UserSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -20,31 +20,53 @@ class UserSeeder extends Seeder
             'password' => Hash::make("ali12345"),
             'full_name' => "Ali Abdelaal",
             'bio' => "Full Stack Web Developer",
-            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+            'profile_image' => "Default_pfp.jpg"
         ]);
+
         User::create([
             'username' => "Ahmad",
             'email' => "ahmad@gmail.com",
             'password' => Hash::make("ahmad12345"),
             'full_name' => "Ahmad Farou",
             'bio' => "Full Stack Web Developer",
-            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+            'profile_image' => "Default_pfp.jpg"
         ]);
+
         User::create([
             'username' => "Mhamad",
             'email' => "mhamad@gmail.com",
             'password' => Hash::make("mhamad12345"),
             'full_name' => "Mhamad Ballan",
             'bio' => "Full Stack Web Developer",
-            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+            'profile_image' => "Default_pfp.jpg"
         ]);
+
         User::create([
             'username' => "Hsein",
             'email' => "hsein@gmail.com",
             'password' => Hash::make("hsein12345"),
             'full_name' => "Hsein Taktak",
             'bio' => "Full Stack Web Developer",
-            'profile_image' => "backend\storage\app\public\default_profile\Default_pfp.jpg"
+            'profile_image' => "Default_pfp.jpg"
         ]);
+
+        User::create([
+            'username' => "3laych",
+            'email' => "3laych@gmail.com",
+            'password' => Hash::make("3laych12345"),
+            'full_name' => "Ali Abdelaal",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "Default_pfp.jpg"
+        ]);
+
+        User::create([
+            'username' => "ITS_WICK",
+            'email' => "wick@gmail.com",
+            'password' => Hash::make("wick12345"),
+            'full_name' => "Ahmad Kabalan",
+            'bio' => "Full Stack Web Developer",
+            'profile_image' => "Default_pfp.jpg"
+        ]);
+
     }
 }
