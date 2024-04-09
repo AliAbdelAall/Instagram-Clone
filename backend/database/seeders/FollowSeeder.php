ab80587ae8a80182c6a4b445ffb8a088843d9214
AliAbdelAall <3laych@gmail.com>
2024-04-09 03:41:19 +0300

    added FollowSeeder

--- backend/database/seeders/FollowSeeder.php
+++ backend/database/seeders/FollowSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Follow;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,55 @@ class FollowSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Follow::create([
+            'follower_id' => 1, 
+            'followed_id' => 2
+        ]);
+
+        Follow::create([
+            'follower_id' => 1, 
+            'followed_id' => 3
+        ]);
+
+        Follow::create([
+            'follower_id' => 1, 
+            'followed_id' => 4
+        ]);
+
+        Follow::create([
+            'follower_id' => 2, 
+            'followed_id' => 1
+        ]);
+
+        Follow::create([
+            'follower_id' => 2, 
+            'followed_id' => 3
+        ]);
+
+        Follow::create([
+            'follower_id' => 3, 
+            'followed_id' => 1
+        ]);
+
+        Follow::create([
+            'follower_id' => 3, 
+            'followed_id' => 2
+        ]);
+
+        Follow::create([
+            'follower_id' => 4, 
+            'followed_id' => 1
+        ]);
+
+        Follow::create([
+            'follower_id' => 5, 
+            'followed_id' => 1
+        ]);
+
+        Follow::create([
+            'follower_id' => 6, 
+            'followed_id' => 1
+        ]);
+
     }
 }
