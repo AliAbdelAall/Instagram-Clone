cf96545edc0076ce91cc24e5ac38c23ba42c237c
AliAbdelAall <3laych@gmail.com>
2024-04-10 16:14:57 +0300

    added Profile component

--- frontend/src/pages/Main/Feed/components/FeedProfile/index.jsx
+++ frontend/src/pages/Main/Feed/components/FeedProfile/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {useNavigate, Link} from "react-router-dom"
 
 const FeedProfile = ({profileImage, username, fullName, handleLogout}) => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='flex align-center space-between '>
       <div className='flex center profile-feed'>
-        <img className='profile-image' src={profileImage} width={44} hight={44} alt="profile" />
+        <Link
+        to = "/profile"
+        >
+        <img className='profile-image' src={profileImage} width={44} hight={44} alt="profile"/>
+        </Link>
         <div className='flex column user-name'>
           <p className='text-sm'>{username}</p>
           <p className='text-sm text-gray'>{fullName}</p>
--- frontend/src/pages/Main/Profile/index.jsx
+++ frontend/src/pages/Main/Profile/index.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile-container'>
+      <div className='flex profile-wrapper'>
+        <img src="#" alt="profile_image" />
+
+        <div className='flex column'>
+
+          <div>
+            <p>username</p>
+            <button>Edit profile</button>
+            <button>Log out</button>
+          </div>
+
+          <div>
+            <p>posts</p>
+            <p>followers</p>
+            <p>following</p>
+          </div>
+
+          <div>
+            <p>Full Name</p>
+            <p>Bio</p>
+          </div>
+        </div>
+        <div>
+          <p>POSTS</p>
+          <p>Add post</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
